5f9327b1b1f3604c732ae51529fff3b3340ab567
EmmaHenault <emmahenault31@gmail.com>
2024-02-29 15:53:55 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Bonjour';
+
+  public isAdmin: boolean = false;
+
+toggleAdmin(){
+ this.isAdmin = !this.isAdmin;
+}
+  
+
+  
+}
